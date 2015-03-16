dec3c69a2620debbba5d6b4698f7a85d6323e253
Kelsey Ando <kma3a@virginia.edu>
2015-03-16 17:34:06 -0400

    adding the rest of the function to create the deck

--- card.js
+++ card.js
@@ -11,6 +11,12 @@ function createDeck() {
 	var suits = ["harts", "spades", "dimonds","clubs"];
 	var numbers = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten", "jack", "queen","king"];
 	var deck = [];
-	
+	suits.forEach(function(suit) {
+		numbers.forEach(function(number) {
+			deck.push(new Card(suit, number));
+		})
+	})
+	return deck;
 }
 
+console.log(createDeck().length === 52);
