5e0411c711cfb8739d8a26d0dc1f44ad4c1e6562
Kelsey Ando <kma3a@virginia.edu>
2015-03-16 17:29:32 -0400

    adding an object for the card and the start of a function to create the deck

--- card.js
+++ card.js
@@ -1,1 +1,16 @@
-var suits = ["harts", "spades", "dimonds","clubs"];
+function Card(value, suit) {
+	this.value = value;
+	this.suit = suit;
+}
+
+Card.prototype.view = function() {
+		console.log( this.value + " of " + this.suit);
+	}
+
+function createDeck() {
+	var suits = ["harts", "spades", "dimonds","clubs"];
+	var numbers = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten", "jack", "queen","king"];
+	var deck = [];
+	
+}
+
